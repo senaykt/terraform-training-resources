b5e1dece4a0c75397b25ed1fbf549d8b6d3dc19a
Sena Yakut <42169819+senaykt@users.noreply.github.com>
2025-07-30 07:50:21 +0300

    Update outputs.tf

--- terraform-ec2/outputs.tf
+++ terraform-ec2/outputs.tf
@@ -5,3 +5,8 @@ output "instance_id" {
 output "public_ip" {
   value = aws_instance.demo_ec2.public_ip
 }
+
+output "private_key_pem" {
+  value     = tls_private_key.demo.private_key_pem
+  sensitive = true
+}
