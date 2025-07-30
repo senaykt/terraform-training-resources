e49bba8bbb640c8a795f0ec0dc2d9d4535f41e90
Sena Yakut <42169819+senaykt@users.noreply.github.com>
2025-07-30 12:53:53 +0300

    Update main.tf

--- terraform-ec2/main.tf
+++ terraform-ec2/main.tf
@@ -29,6 +29,13 @@ resource "aws_security_group" "demo_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
