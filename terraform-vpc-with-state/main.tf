6cfdb91ea2e36170e2a52e5079ec56a0e477b05f
Sena Yakut <42169819+senaykt@users.noreply.github.com>
2025-07-31 15:02:39 +0300

    Update main.tf

--- terraform-vpc-with-state/main.tf
+++ terraform-vpc-with-state/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = var.aws_region
+  region = "eu-west-2"
 }
 
 terraform {
@@ -34,7 +34,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "${var.aws_region}a"
+  availability_zone       = "eu-west-2a"
 
   tags = {
     Name = "public-subnet"
@@ -44,7 +44,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "${var.aws_region}a"
+  availability_zone = "eu-west-2a"
 
   tags = {
     Name = "private-subnet"
